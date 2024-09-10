44d5c40c553fba982000dcb2009c0bfe8783d5a4
haoliu <328937314@qq.com>
2024-09-10 22:57:34 +0800

    222

---
+++ c++对象模型和this指针/c++对象模型和this指针/03 空指针访问成员函数.cpp
@@ -0,0 +1,41 @@
+//#define  _CRT_SECURE_NO_WARNINGS 1
+//#include<iostream>
+//using namespace std;
+//
+////空指针调用成员函数
+//
+//class Person
+//{
+//public:
+//
+//	void showClassName()
+//	{
+//		cout << "this is Person class" << endl;
+//	}
+//
+//	void showPersonAge()
+//	{
+//		/*//报错原因是因为传入的指针为NULL
+//		cout << "age = " << m_Age << endl;*/
+//		if(this == NULL)
+//		{
+//			return;
+//		}
+//		cout << "age = " << m_Age << endl;
+//	}
+//
+//	int m_Age;
+//};
+//
+//void test01()
+//{
+//	Person * p = NULL;
+//	//p->showClassName();
+//	p->showPersonAge();
+//}
+//
+//int main(){
+//	test01();
+//	system("pause");
+//	return 0;
+//}
